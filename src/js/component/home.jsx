5adc3e23023eaa42a3eb06d7483696a83e2a1b03
Stephania31 <acunaantunez@gmail.com>
2023-03-02 02:58:11 +0000

    agregue tareas pero queda pendiente quitarlas y el contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+import "../../styles/styles.css";
+
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const arrTemp = ["Ir al super", "lavar el auto"];
+
+  return (
+    <div className="container justify-content-center align-items-center">
+      <div className="row d-flex justify-content-center">
+        <h1 className="row d-flex justify-content-center"> todos </h1>
+        <input placeholder="What needs to be done?" />
+      </div>
+      <div className="row d-flex justify-content-center">
+	  {arrTemp && arrTemp.length > 0 ?
+				    <>{arrTemp.map((item, index)=> { //Funcion callback
+					return<li key={index} className="d-flex justify-content-between">
+						{item} - {item.done ? "Realizada" : "Por Hacer"}
+						<button 
+						   className="ocultar" 
+						   type="button"
+						   onClick={() => {
+							    eliminarTarea(index)
+						}}					
+					>
+							<i class="fas fa-trash-alt"></i>
+					</button>
+					</li>
+					})}</>
+				:
+				<><h1>No pending tasks</h1></>
+				}
+	
+
+
+      </div>
+    </div>
+  );
 };
 
 export default Home;
