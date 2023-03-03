e554cf2020245ed26af3f46c9fde0f8ce01f78d2
Stephania31 <acunaantunez@gmail.com>
2023-03-03 13:11:16 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,7 +7,7 @@ import "../../styles/styles.css";
 
 //create your first component
 const Home = () => {
-  const [arrTemp, setArrtemp] =useState(["Ir al super", "lavar el auto"]) ;
+  const [arrTemp, setArrtemp] =useState(["Yoga class", "Lunch with Martin", "Buy dog food"]) ;
   const eliminarTarea = (indice) => {
 	setArrtemp(
 		arrTemp.filter((item, index)=> {
@@ -18,22 +18,23 @@ const Home = () => {
   
 
   return (
-    <div className="container justify-content-center align-items-center">
-      <div className="row d-flex justify-content-center">
-        <h1 className="row d-flex justify-content-center"> todos </h1>
-        <input placeholder="What needs to be done?" 
+    <div className="container justify-content-center align-items-center mt-4 ">
+    
+        <h1 className="titulo row d-flex justify-content-center mb-4 text-danger text-opacity-25 display-1"> to do list </h1>
+		<div className="row d-flex justify-content-center  my-4 shadow-lg p-3 mb-5">
+		<input ype="text" className="form-control form-control-lg rounded-0 text-secondary" name="search"placeholder="What needs to be done?" 
 		onKeyDown={(e)=>{
 			if( e.keyCode=="13"){
 				setArrtemp([...arrTemp, e.target.value])
 			}
 		}}
 		/>
-      </div>
-      <div className="row d-flex justify-content-center">
+      
+      <div className="row d-flex justify-content-center list-group-item rounded-0">
 	  {arrTemp && arrTemp.length > 0 ?
 				    <>{arrTemp.map((item, index)=> { 
-					return<li key={index} className="d-flex justify-content-between">
-						{item} - {item.done ? "Realizada" : "Por Hacer"}
+					return<li key={index} className="d-flex justify-content-between align-items-center form-control rounded-0 text-secondary">
+						{item}  {item.done ? "Realizada" : ""}
 						<button 
 						   className="ocultar" 
 						   type="button"
@@ -41,18 +42,19 @@ const Home = () => {
 							    eliminarTarea(index)
 						}}					
 					>
-							<i class="fas fa-trash-alt"></i>
+							<i class="fas fa-times"></i>
 					</button>
 					</li>
 					})}</>
 				:
-				<><h1>There are no items, add items</h1></>
+				<><h4 className="text-secondary text-opacity-50">There are no items.<span className="text-danger text-opacity-25"> Add items</span></h4></>
 				}
 	
 
 
       </div>
-	  <h6>{arrTemp.length} item left</h6>
+	  			<h6 className="d-flex justify-content-between align-items-center form-control rounded-0 text-secondary"><small>{arrTemp.length} item left</small></h6>
+				  </div> 
     </div>
   );
 };
